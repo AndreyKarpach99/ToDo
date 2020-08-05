df96035e57b8fa9b98f922385c78539598464c20
Andreykas01 <andreykarpach99@gmail.com>
2020-08-06 00:02:46 +0300

    first commit

---
+++ precache-manifest.59c615f7bb8492a7e6a69d208aae735b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11310b1f1ce2d2e2ce5e76c8a2e1dbf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f33c38fe5b29f130f2",
+    "url": "/static/css/main.83cdca98.chunk.css"
+  },
+  {
+    "revision": "3fe9f8a1d4fade360c43",
+    "url": "/static/js/2.731454ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.731454ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f33c38fe5b29f130f2",
+    "url": "/static/js/main.49c91a29.chunk.js"
+  },
+  {
+    "revision": "759e81ee1e3f967c5c66",
+    "url": "/static/js/runtime-main.66c35898.js"
+  }
+]);
